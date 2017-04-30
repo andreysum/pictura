635a724e63681be9d632149d38da567e63e5fe0a
andreysum <felix2k@tut.by>
2017-04-30 21:07:28 +0300

    * сайт переведен на новый движок Symfony 2.8

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,9 +5,12 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Yaml;
 
 class DefaultController extends Controller
 {
+
+    private $baseDir = '';
     /**
      * @Route("/", name="homepage")
      */
@@ -36,10 +39,58 @@ class DefaultController extends Controller
 
         );
 
+        $postContents = $this->getPostContents();
+        $postTexts = $postContents['index']['posts'];
+        $imageNames = $this->getPostImageNames();
+        $posts = array();
+
+        for ($i = 0; $i < 13; $i++) {
+            $postItem = array();
+            if (isset($imageNames[$i])) {
+                $imageName = $imageNames[$i];
+                $postItem['type'] = (strpos($imageName, 'c_') === 0) ? 'c_img' : 'img';
+                $postItem['content'] = $imageName;
+            } else {
+                $postText = array_shift($postTexts);
+                $postItem['type'] = 'text';
+                $postItem['content'] = ($postText === null) ? '' : $postText;
+            }
+            $posts[$i] = $postItem;
+        }
+        shuffle($posts);
+        for ($i = 0; $i < 13; $i++) {
+            $postItem = $posts[$i];
+            if ($postItem['type'] == 'c_img') {
+                $replacedItem = $posts[5];
+                $posts[5] = $postItem;
+                $posts[$i] = $replacedItem;
+            }
+        }
         // replace this example code with whatever you need
+        $this->baseDir = realpath($this->container->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR;
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => $this->baseDir,
             'menu' => $menu,
+            'posts' => $posts
         ));
+    }
+
+    private function getPostContents() {
+        return json_decode(file_get_contents($this->baseDir.'bundles/app/content.json'), true);
+    }
+
+    private function getPostImageNames() {
+        $imageNames = array();
+        if ($handle = opendir($this->baseDir.'bundles/app/images/index')) {
+            while (false !== ($entry = readdir($handle))) {
+
+                if ($entry != "." && $entry != ".." && (preg_match("/^[^\.]+\.(?:jpg|png|gif|jpeg)$/i", $entry) == 1)) {
+                    $imageNames[] = $entry;
+                }
+            }
+            closedir($handle);
+        }
+
+        return $imageNames;
     }
 }
--- src/AppBundle/Resources/public/js/global.js
+++ src/AppBundle/Resources/public/js/global.js
@@ -6,13 +6,14 @@ jQuery(document).ready(function($) {
 
 	$blocks.imagesLoaded(function(){
 		$blocks.masonry({
-			itemSelector: '.post-container'
+			itemSelector: '.post-container',
+			columnWidth: 290
 		});
 
 		// Fade blocks in after images are ready (prevents jumping and re-rendering)
 		$(".post-container").fadeIn();
 	});
-	
+
 	$(document).ready( function() { setTimeout( function() { $blocks.masonry(); }, 500); });
 
 	$(window).resize(function () {
@@ -76,31 +77,32 @@ jQuery(document).ready(function($) {
 	// When Jetpack Infinite scroll posts have loaded
 	$( document.body ).on( 'post-load', function () {
 
-		var $container = $('.posts');
-		$container.masonry( 'reloadItems' );
+		//var $container = $('.posts');
+		//$container.masonry( 'reloadItems' );
 		
-		$blocks.imagesLoaded(function(){
+		/*$blocks.imagesLoaded(function(){
 			$blocks.masonry({
-				itemSelector: '.post-container'
+				itemSelector: '.post-container',
+				columnWidth: 290
 			});
 	
 			// Fade blocks in after images are ready (prevents jumping and re-rendering)
 			$(".post-container").fadeIn();
-		});
+		});*/
 		
 		// Rerun video resizing
 		resizeVideo(vidSelector);
 		
-		$container.masonry( 'reloadItems' );
+		//$container.masonry( 'reloadItems' );
 		
 		// Load Flexslider
-	    $(".flexslider").flexslider({
+	    /*$(".flexslider").flexslider({
 	        animation: "slide",
 	        controlNav: false,
 	        prevText: "Previous",
 	        nextText: "Next",
 	        smoothHeight: true   
-	    });
+	    });*/
 		
 		$(document).ready( function() { setTimeout( function() { $blocks.masonry(); }, 500); });
 
